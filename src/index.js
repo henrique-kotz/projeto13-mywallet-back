b422d9a2242ad06a39db150175ff44127ab444d8
Henrique Butzke Kotz <henriquebkotz@gmail.com>
2022-05-07 19:11:15 -0300

    user sign-up

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express, { json } from 'express';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
+import joi from 'joi';
+import bcrypt from 'bcrypt';
 
 const app = express();
 app.use(cors());
@@ -13,6 +15,45 @@ let db;
 mongoClient.connect(() => {
     db = mongoClient.db('my-wallet');
     console.log('Connected with my-wallet database');
+});
+
+app.post('/sign-up', async (req, res) => {
+    const register = req.body;
+    console.log(req.body);
+    const { username, email, password } = req.body;
+    const registerSchema = joi.object({
+        username: joi.string().alphanum().required(),
+        email: joi.string().email().required(),
+        password: joi.string().required(),
+        repeat_password: joi.ref('password')
+    });
+
+    const { error } = registerSchema.validate(register, { abortEarly: false });
+    if (error || password !== register.repeat_password) {
+        res.sendStatus(422)
+        return;
+    }
+
+    try {
+        const userCollection = db.collection('users');
+
+        const usernameConflict = await userCollection.findOne({ username });
+        const emailConflict = await userCollection.findOne({ email });
+        if (usernameConflict || emailConflict) {
+            res.sendStatus(409);
+            return;
+        }
+
+        const passwordHash = bcrypt.hashSync(password, 10);
+        await userCollection.insertOne({
+            username, email,
+            password: passwordHash
+        });
+        res.sendStatus(201);
+    } catch(err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
 });
 
 
